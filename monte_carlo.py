d39dd74488e33e245f507f30149894fd69c63b90
Toby Crisford <tobycrisford@gmail.com>
2026-01-17 20:13:49 +0000

    Adapting lattice class so it can simulate an O(2) model as well

--- monte_carlo.py
+++ monte_carlo.py
@@ -7,27 +7,37 @@ from matplotlib.animation import FuncAnimation
 
 class Lattice:
 
-    STATES = np.array([-1, 1])
+    ISING_STATES = np.array([0.0, np.pi])
+    O2_STATES = np.linspace(0, 2*np.pi, endpoint=False, num=20)
 
-    def __init__(self, dimension: int, N: int, beta: float):
+    def __init__(self, dimension: int, N: int, beta: float, o_dimension: int):
 
-        self.lattice = (np.random.rand(*[N for _ in range(dimension)]) > 0.5) * 2 - 1
+        if o_dimension == 1:
+            self.lattice = (np.random.rand(*[N for _ in range(dimension)]) > 0.5) * np.pi
+            self.states = self.ISING_STATES
+        elif o_dimension == 2:
+            self.lattice = np.random.rand(*[N for _ in range(dimension)]) * 2 * np.pi
+            self.states = self.O2_STATES
+        else:
+            raise NotImplementedError()
+
         self.beta = beta
         self.N = N
         self.dimension = dimension
+        self.o_dimension = o_dimension
 
     def _compute_energy_term(self, sample_point: np.ndarray, dim_index: int, neighbour_diff: int) -> float:
 
         sample_point[dim_index] = (sample_point[dim_index] + neighbour_diff) % self.N
-        energy_vals = self.STATES * self.lattice[tuple(sample_point)]
+        energy_vals = -1 * np.cos(self.states - self.lattice[tuple(sample_point)])
         sample_point[dim_index] = (sample_point[dim_index] - neighbour_diff) % self.N
-        return -1 * energy_vals
+        return energy_vals
 
     def monte_carlo_update(self) -> None:
 
         sample_point = np.random.randint(0, self.N, size=self.dimension)
 
-        energy_vals = np.zeros(2)
+        energy_vals = np.zeros(len(self.states))
         for d in range(self.dimension):
             energy_vals += self._compute_energy_term(sample_point, d, 1)
             energy_vals += self._compute_energy_term(sample_point, d, -1)
@@ -35,10 +45,7 @@ class Lattice:
         probs = np.exp(-1 * self.beta * energy_vals)
         probs /= np.sum(probs)
 
-        if np.random.rand() < probs[0]:
-            self.lattice[tuple(sample_point)] = -1
-        else:
-            self.lattice[tuple(sample_point)] = 1
+        self.lattice[tuple(sample_point)] = self.states[np.random.choice(len(self.states), p=probs)]
 
 
     def run_n_updates(self, n: int, progress: bool = False) -> None:
@@ -52,7 +59,7 @@ class Lattice:
     def get_image_lattice(self) -> np.ndarray:
 
         if self.dimension == 2:
-            return self.lattice > 0
+            return self.lattice
         elif self.dimension == 1:
             img_lattice = np.zeros((self.N, self.N))
             img_lattice[:, :] = self.lattice
@@ -79,7 +86,7 @@ def animate_model(lattice: Lattice, n_iterations: int, refresh_every: int) -> No
 
 if __name__ == '__main__':
 
-    test_lattice = Lattice(1, 100, 100)
+    test_lattice = Lattice(2, 100, 10, 2)
 
     animate_model(test_lattice, 1000000, 10000)
 
