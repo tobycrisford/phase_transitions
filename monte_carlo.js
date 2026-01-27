ee9e110306f948272e802cba576340ce3d88438a
Toby Crisford <tobycrisford@gmail.com>
2026-01-27 18:06:35 +0000

    Introducing loading message for dynamic explainer and explanation caching

--- monte_carlo.js
+++ monte_carlo.js
@@ -264,13 +264,18 @@ function fetch_parameters() {
 
 let dynamic_explainer_promise = fetch('dynamic_explainer.json').then((response) => response.json());
 
+const explanation_cache = {};
+
 async function update_dynamic_explanation(o_dimension, dimension, temperature, explanation_element) {
     const dynamic_explainer_lookup = await dynamic_explainer_promise;
     const exps = dynamic_explainer_lookup[o_dimension.toString()][dimension.toString()];
     for (const exp of exps) {
         if (((temperature >= exp.range[0]) || (exp.range[0] === null)) && ((temperature < exp.range[1]) || (exp.range[1] === null))) {
-            const explanation = await fetch('dynamic_explainer/' + exp.explainer).then((response) => response.text());
-            explanation_element.innerHTML = explanation;
+            if (!(exp.explainer in explanation_cache)) {
+                console.log('fetching text file');
+                explanation_cache[exp.explainer] = await fetch('dynamic_explainer/' + exp.explainer).then((response) => response.text());
+            }
+            explanation_element.innerHTML = explanation_cache[exp.explainer];;
         }
     }
 }
@@ -300,6 +305,7 @@ function update_parameters() {
     const temperature = 1 / parameters.beta;
     document.getElementById("temperature_display").textContent = temperature.toFixed(2).toString();
     document.getElementById("grid_display").textContent = lattice.N.toString();
+    document.getElementById("dynamic_explainer").innerHTML = "<i>Loading</i>"
     update_dynamic_explanation(parameters.o_dimension, parameters.dimension, temperature, document.getElementById("dynamic_explainer"));
 
     if (lattice_change || animation_id === null) {
